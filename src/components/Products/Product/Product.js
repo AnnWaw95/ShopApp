a85fa5d7658d580be9747792947d0a95a55f6609
Ania Wawerek <wawerekanna@gmail.com>
2022-04-16 11:40:16 +0200

    12 Remove bugf from OptionSize and add useMemo

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,49 +1,48 @@
 import styles from './Product.module.scss';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import ProductImage from './ProductImage/ProductImage';
 import ProductForm from './ProductForm/ProductForm';
 
 
 const Product = ({ title, basePrice, colors, sizes, name }) => {
 
+const handleSubmit = e => {
+    e.preventDefault();
+    console.log('Summary')
+    console.log('==============')
+    console.log('Name:' + title)
+    console.log('Price:' + getPrice)
+    console.log('Size:' + currentSize)
+    console.log('Color:' + currentColor)
+  }
 
   const [currentColor, setCurrentColor] = useState(colors[0]);
   const [currentSize, setCurrentSize] = useState(sizes[0].name);
 
 
-  const getPrice = () => {
+  const getPrice = useMemo(() => {
     const found = sizes.find(element => element.name === currentSize)
-
-    return basePrice + found.additionalPrice
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    console.log('Summary')
-    console.log('==============')
-    console.log('Name:' + title)
-    console.log('Price:' + getPrice())
-    console.log('Size:' + currentSize.name)
-    console.log('Color:' + currentColor)
-  }
+    return basePrice + found.additionalPrice;
+  }, [currentSize, basePrice, sizes]);
   
   return (
     <article className={styles.product}>
       <ProductImage name={name} title={title} currentColor={currentColor} />
       <div>
-      <header>
-        <h2 className={styles.name}>{title}</h2>
-        <span className={styles.price}>Price: {getPrice()} $</span>
-      </header>
-        <ProductForm
-          handleSubmit={handleSubmit} 
-          colors={colors} 
-          sizes={sizes} 
-          currentColor={currentColor} 
-          currentSize={currentSize} 
-          setCurrentColor={setCurrentColor} 
-          setCurrentSize={setCurrentSize} />
+        <header>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {getPrice} $</span>
+        </header>
+          <ProductForm
+            handleSubmit={handleSubmit}
+            price={getPrice} 
+            colors={colors} 
+            sizes={sizes} 
+            currentColor={currentColor} 
+            currentSize={currentSize} 
+            setCurrentColor={setCurrentColor} 
+            setCurrentSize={setCurrentSize} />
       </div>
     </article>
   )
--- src/components/Products/Product/ProductForm/OptionSize/OptionSize.js
+++ src/components/Products/Product/ProductForm/OptionSize/OptionSize.js
@@ -8,12 +8,8 @@ const OptionSize = ({sizes, setCurrentSize, currentSize}) => {
     return(
         <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {sizes.map( size =>
-                <li key={shortid()}>
-                  <button type="button" onClick={() => setCurrentSize(size)} className={clsx(currentSize === size.name && styles.active)}>{size.name}</button>
-                </li>
-              )}
+            <ul className={styles.choices}> 
+                  {sizes.map(size => <li key={shortid()}><button type="button" onClick={() => setCurrentSize(size.name)} className={clsx(currentSize === size.name && styles.active)}>{size.name}</button></li>)}
             </ul>
         </div>
     );
